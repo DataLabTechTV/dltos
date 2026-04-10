fcbb0df60bf5b568ef30038dafcd3acbca97da64
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-04-10 10:20:50 +0100

    feat(languages): add python3 headers required to build some libs

--- build_files/10-languages.sh
+++ build_files/10-languages.sh
@@ -10,6 +10,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/cargo-env.sh"
 
 dnf5 -y install golang cargo uv nodejs-npm
 dnf5 -y install golangci-lint delve
+dnf5 -y install python3-devel
 
 go install golang.org/x/tools/gopls@latest
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
