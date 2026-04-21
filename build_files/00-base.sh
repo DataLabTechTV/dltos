2c53878bbb2f7a3c71a39ab2c09069d2e239d316
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-04-21 17:13:15 +0100

    fix: rollback all changes, as this is a terra copr issue

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -2,19 +2,14 @@
 
 set -euxo pipefail
 
-rpm --import https://repos.fyralabs.com/terra$(rpm -E %fedora)/key.asc
-dnf5 -y --repofrompath 'terra-repo,https://repos.fyralabs.com/terra$releasever' install terra-release
-
 dnf5 -y remove xwaylandvideobridge
 dnf5 -y install kitty
 dnf5 -y install xdg-desktop-portal-gnome qt6ct
 dnf5 -y install wev wlsunset cava playerctl
-dnf5 -y install mpvpaper
+dnf5 -y --enable-repo=terra install mpvpaper
 
 dnf5 -y copr enable avengemedia/dms
 dnf5 -y install niri
 dnf5 -y copr disable avengemedia/dms
 
-dnf5 -y install noctalia-shell
-
-dnf5 config-manager setopt terra.enabled=false
+dnf5 -y --enable-repo=terra install noctalia-shell
