af5d8b0aa5664b77e90669cbd91d8cc9d2fc1926
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-04-25 21:31:18 +0100

    feat(niri): bump up to 26.04

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -8,8 +8,8 @@ dnf5 -y install xdg-desktop-portal-gnome qt6ct
 dnf5 -y install wev wlsunset cava playerctl
 dnf5 -y --enable-repo=terra install mpvpaper
 
-dnf5 -y copr enable avengemedia/dms
+dnf5 -y copr enable yalter/niri
 dnf5 -y install niri
-dnf5 -y copr disable avengemedia/dms
+dnf5 -y copr disable yalter/niri
 
 dnf5 -y --enable-repo=terra install noctalia-shell
