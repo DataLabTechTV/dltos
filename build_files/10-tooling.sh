da36d95b0e9c03a71359cb3699d25acc996c9e8b
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-05-04 22:18:14 +0100

    fix(tooling): systemd is not started at the time of nix installation

--- build_files/10-tooling.sh
+++ build_files/10-tooling.sh
@@ -10,6 +10,6 @@ DLTOS_DIR="$(dirname "${BASH_SOURCE[0]}")/dltos"
 
 dnf5 -y install uv firecracker
 
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm
+curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm --no-start-daemon
 
 uv tool install ramalama
