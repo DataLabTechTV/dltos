d728d1165f115d62a08aa4e19065e1bbb1ce0947
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-05-04 22:37:30 +0100

    fix(tooling): switch back to dnf install for nix but add nix-daemon

--- build_files/10-tooling.sh
+++ build_files/10-tooling.sh
@@ -10,7 +10,7 @@ DLTOS_DIR="$(dirname "${BASH_SOURCE[0]}")/dltos"
 
 dnf5 -y install uv firecracker
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |
-	sh -s -- install linux --no-start-daemon --no-confirm
+dnf5 -y install nix nix-daemon
+systemctl enable --now nix-daemon.socket
 
 uv tool install ramalama
