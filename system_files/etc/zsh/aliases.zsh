097cf4f03135005571bd35d9e6f6e08e6cda531c
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-05-13 18:09:33 +0100

    feat(zsh): add system-wide configs

---
+++ system_files/etc/zsh/aliases.zsh
@@ -0,0 +1,23 @@
+unalias ls 2>/dev/null
+
+alias ff='fastfetch'
+alias v='vim'
+alias pbcopy='wl-copy --type text/plain'
+alias pbpaste='wl-paste'
+alias ping='prettyping'
+
+# --- Distrobox ---
+alias d='distrobox'
+
+# --- Chezmoi ---
+alias c='chezmoi'
+
+# --- Git ---
+alias gsw='git update-index --skip-worktree'
+alias gnsw='git update-index --no-skip-worktree'
+alias gswls='git ls-files -v | grep ^S'
+
+# --- Kubernetes ---
+alias m='minikube'
+alias k='kubectl'
+alias kns='kubens'
---
+++ system_files/etc/zsh/env.zsh
@@ -0,0 +1,37 @@
+# --- General ---
+export SHELL=/bin/zsh
+export EDITOR=vim
+export VISUAL=$EDITOR
+export PAGER='less -X -F -i'
+
+# --- Z Shell ---
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=10000
+SAVEHIST=100000
+WORDCHARS="${WORDCHARS//\//}"
+
+# --- Path ---
+typeset -U path
+path=(
+	$HOME/.local/bin
+	$HOME/go/bin
+	$HOME/.cargo/bin
+	$HOME/.pyenv/bin
+	$HOME/.cabal/bin
+	$HOME/.config/emacs/bin
+	/usr/local/go/bin
+	$path
+)
+
+# --- pyenv ---
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+export CLOUDSDK_PYTHON="$(command -v python)"
+
+# --- podman ---
+if command -v podman >/dev/null 2>&1; then
+	export DOCKER_HOST="unix://$(podman info --format '{{.Host.RemoteSocket.Path}}')"
+	export REGISTRY_AUTH_FILE="$HOME/.config/containers/auth.json"
+fi
+
+# --- homebrew ---
+export HOMEBREW_NO_ENV_HINTS=1
