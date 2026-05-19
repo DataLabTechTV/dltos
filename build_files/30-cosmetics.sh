146aa49591f3042228cb2dcf52c1e4c2f59719c6
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-05-19 11:32:51 +0100

    fix(cosmetics): missing input arg

--- build_files/30-cosmetics.sh
+++ build_files/30-cosmetics.sh
@@ -3,4 +3,5 @@
 set -euxo pipefail
 
 mv /etc/profile.d/bazzite-neofetch.sh /etc/profile.d/dltos-neofetch.sh
-sed -i "s|/usr/share/ublue-os/bazzite/fastfetch.jsonc|/usr/share/dltos/fastfetch.jsonc|g"
+sed -i "s|/usr/share/ublue-os/bazzite/fastfetch.jsonc|/usr/share/dltos/fastfetch.jsonc|g" \
+	/etc/profile.d/dltos-neofetch.sh
