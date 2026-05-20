c003d627ec604af2e6acbb94c3c414eca6c2e5ba
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-05-20 10:32:01 +0100

    feat(zsh): enable completion cache

--- system_files/etc/zshrc.d/40-completion.zsh
+++ system_files/etc/zshrc.d/40-completion.zsh
@@ -5,3 +5,5 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' rehash true
 zstyle ':completion:*' special-dirs true
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.cache/zsh/completion
