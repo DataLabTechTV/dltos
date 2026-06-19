64875d3a0c41e9c9297e86e47b23ed7c71e5af9a
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-06-19 11:02:46 +0100

    fix(services): wrong selinux context for /usr/bin/bat

--- build_files/20-services.sh
+++ build_files/20-services.sh
@@ -2,5 +2,8 @@
 
 set -euxo pipefail
 
+semanage fcontext -a -t bin_t '/usr/bin/bat'
+restorecon -v /usr/bin/bat
 systemctl enable bat-cache-build.service
+
 systemctl --global enable niri-float-sticky.service
