30381e388083d71218524075a60fc6baf82cf0b1
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-06-19 12:49:01 +0100

    feat(zsh): add p alias for podman and cleanup file

--- system_files/etc/zshrc.d/50-aliases.zsh
+++ system_files/etc/zshrc.d/50-aliases.zsh
@@ -1,17 +1,15 @@
 unalias ls 2>/dev/null
 
 abbrev-alias ff='fastfetch'
-abbrev-alias v='vim'
 abbrev-alias pp='prettyping'
+
+abbrev-alias v='vim'
+abbrev-alias d='distrobox'
+abbrev-alias c='chezmoi'
+abbrev-alias p='podman'
 
 alias pbcopy='wl-copy --type text/plain'
 alias pbpaste='wl-paste'
-
-# --- Distrobox ---
-abbrev-alias d='distrobox'
-
-# --- Chezmoi ---
-abbrev-alias c='chezmoi'
 
 # --- Git ---
 alias gsw='git update-index --skip-worktree'
@@ -23,5 +21,5 @@ abbrev-alias m='minikube'
 abbrev-alias k='kubectl'
 abbrev-alias kns='kubens'
 
-# --- Global ---
+# --- Global Aliases ---
 abbrev-alias -g N='&>/dev/null'
