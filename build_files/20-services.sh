a32352af4864547f79b7f8b94a56d97a9e683e3f
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-06-19 11:13:46 +0100

    style(service): no quotes needed

--- build_files/20-services.sh
+++ build_files/20-services.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-semanage fcontext -a -t bin_t '/usr/bin/bat'
+semanage fcontext -a -t bin_t /usr/bin/bat
 restorecon -v /usr/bin/bat
 systemctl enable bat-cache-build.service
 
