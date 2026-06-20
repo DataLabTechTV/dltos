74854571d645d8469a1c2b03bbae536f3364a7c6
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-06-20 22:03:55 +0100

    feat(tooling): add trash-cli to use with yazi or the cli directly

--- build_files/10-tooling.sh
+++ build_files/10-tooling.sh
@@ -39,7 +39,7 @@ install_shell_tools() {
     dnf5 -y install starship
     dnf5 -y copr disable atim/starship
 
-    dnf5 -y install chezmoi direnv keychain zoxide bat ripgrep fd-find eza ncdu age btop nvtop tldr
+    dnf5 -y install chezmoi direnv keychain zoxide bat ripgrep fd-find eza ncdu age btop nvtop tldr trash-cli
     dnf5 -y --enable-repo=terra install yazi
     go install github.com/pranshuparmar/witr/cmd/witr@latest
 }
