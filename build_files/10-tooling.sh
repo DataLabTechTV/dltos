a2ac06806107e9b69a7bdbeae7476699203145e7
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-06-25 15:58:38 +0100

    fix(tooling): rembg needs python 3.13

--- build_files/10-tooling.sh
+++ build_files/10-tooling.sh
@@ -55,7 +55,7 @@ install_network_tools() {
 
 install_graphics_tools() {
     dnf5 -y install ImageMagick ImageMagick-heic libheif libde265 chafa
-    uv tool install rembg[gpu,cli]
+    uv tool install --python 3.13.14 rembg[gpu,cli]
 }
 
 install_doc_tools() {
