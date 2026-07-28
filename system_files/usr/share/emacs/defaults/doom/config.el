fcd8bfff637c29e6d7797073a5000d6e7c8491da
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-07-28 18:50:07 +0100

    feat(emacs): add big font and set the correct weights

--- system_files/usr/share/emacs/defaults/doom/config.el
+++ system_files/usr/share/emacs/defaults/doom/config.el
@@ -21,7 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font" :size 16 :weight 'medium))
+(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font" :size 16 :weight 'regular)
+      doom-big-font (font-spec :family "CaskaydiaCove Nerd Font" :size 32 :weight 'regular))
      ;; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
