ce23042a0c8ee715cf69ba4b16d959e16a043de2
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-07-30 09:01:04 +0100

    chore(emacs): switch theme to rose pine moon

--- system_files/usr/share/emacs/defaults/doom/config.el
+++ system_files/usr/share/emacs/defaults/doom/config.el
@@ -34,7 +34,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-one)
-(setq doom-theme 'noctalia)
+(setq doom-theme 'doom-rose-pine-moon)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
