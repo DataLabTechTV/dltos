40bd6a6f4b597e37eed42c0a3e6c778f79fe83db
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-09-21 19:03:06 +0100

    fix(tooling): libheif and libde265 seem to be from fedora-multimedia

--- build_files/10-tooling.sh
+++ build_files/10-tooling.sh
@@ -52,7 +52,8 @@ install_mail_tools() {
 }
 
 install_graphics_tools() {
-    dnf5 -y install ImageMagick ImageMagick-heic libheif libde265 chafa
+    dnf5 -y install ImageMagick ImageMagick-heic chafa
+    dnf5 -y --enable-repo=fedora-multimedia libheif libde265
     uv python install 3.13.12
     uv tool install --python 3.13.12 rembg[gpu,cli]
 }
