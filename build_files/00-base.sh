3f6debec594ccc63a6f48211892d5cc0725ad27f
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-09-26 20:26:18 +0100

    fix(steam): temporarily downgrade xwayland-satellite due to steam issue

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -7,6 +7,7 @@ dnf5 -y install zsh
 dnf5 -y install kitty
 
 dnf5 -y remove xwaylandvideobridge
+dnf5 -y downgrade xwayland-satellite-0.8.1-1.fc44
 dnf5 -y install xdg-desktop-portal-gnome qt6ct
 dnf5 -y install wev wlsunset cava playerctl pavucontrol
 dnf5 -y --enable-repo=terra install mpvpaper nwg-look
