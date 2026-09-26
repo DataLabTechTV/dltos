6a3afdc47a1b12d81940d17b7b664d4bbe32a064
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-09-26 21:34:33 +0100

    fix(steam): couldn't downgrade xwayland-satellite before installation

--- build_files/00-base.sh
+++ build_files/00-base.sh
@@ -7,7 +7,11 @@ dnf5 -y install zsh
 dnf5 -y install kitty
 
 dnf5 -y remove xwaylandvideobridge
-dnf5 -y downgrade xwayland-satellite-0.8.1-1.fc44
+
+# Temporary fix, until 0.8.3 is released
+dnf5 -y install xwayland-satellite-0.8.1-1.fc44
+dnf5 versionlock add xwayland-satellite
+
 dnf5 -y install xdg-desktop-portal-gnome qt6ct
 dnf5 -y install wev wlsunset cava playerctl pavucontrol
 dnf5 -y --enable-repo=terra install mpvpaper nwg-look
